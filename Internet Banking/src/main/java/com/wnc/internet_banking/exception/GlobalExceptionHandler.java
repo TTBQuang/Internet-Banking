e1150d948ab492790e7a4985387178c98a753636
21120540 <buuquang102@gmail.com>
2025-04-21 23:00:01 +0700

    Create global exception handler

---
+++ Internet Banking/src/main/java/com/wnc/internet_banking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.wnc.internet_banking.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+        System.out.println(ex.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Collections.singletonMap("message", "Đã xảy ra lỗi không mong muốn"));
+    }
+}
